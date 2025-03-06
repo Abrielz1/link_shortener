80893661ef0ffb8db5ea8934f10761a5301d88d1
Abriel <abrielz11@gmail.com>
2025-03-06 13:25:41 +0300

    ft ft-ttl_and_cash + added optimistic and pessimistic blocking were added

--- src/main/java/ru/effectivemobile/link_shortener/repository/LinkRepository.java
+++ src/main/java/ru/effectivemobile/link_shortener/repository/LinkRepository.java
@@ -1,6 +1,7 @@
 package ru.effectivemobile.link_shortener.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,18 +9,21 @@ import org.springframework.stereotype.Repository;
 import ru.effectivemobile.link_shortener.model.Link;
 import java.time.LocalDateTime;
 import java.util.Optional;
+import jakarta.persistence.LockModeType;
 
 @Repository
 public interface LinkRepository extends JpaRepository<Link, Long> {
     boolean existsByOriginalLink(String originalLink);
 
+    @Lock(LockModeType.OPTIMISTIC)
     @Query(name = """
             SELECT *
             FROM links
-            WHERE original_link = : originalLink            
+            WHERE original_link = : originalLink
             """, nativeQuery = true)
     Optional<Link> getByOriginalLink(@Param("originalLink") String originalLink);
 
+    @Lock(LockModeType.OPTIMISTIC)
     @Query(value = """
         SELECT *
         FROM links
@@ -28,8 +32,9 @@ public interface LinkRepository extends JpaRepository<Link, Long> {
     Optional<Link> getByShortLink(@Param("shortLink") String shortLink);
 
     @Modifying
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query(value = """
-        DELETE 
+        DELETE
         FROM links
         WHERE expired_at <= :itsTimeToDie
                    """, nativeQuery = true)
